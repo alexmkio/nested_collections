8bda9d74af274bfed4ee944ceeb431a3534b9573
Alex Kio <alexandermartelkio@gmail.com>
2022-12-27 17:14:00 -0500

    Do array within hash

--- Examples/array_within_hash.rb
+++ Examples/array_within_hash.rb
@@ -5,6 +5,16 @@ pizza_toppings = {
 }
 
 # 1. what is pizza_toppings.count
+# 3
+p pizza_toppings.count
+
 # 2. what is pizza_toppings.values
+# the three arrays
+p pizza_toppings.values
+
 # 3. how can I access the element “pineapple”
+p pizza_toppings[:fruit][0]
+
 # 4. how can I add the element “olives” to the key “veggies”?
+pizza_toppings[:veggies] << "olives"
+p pizza_toppings
